f18008ae4d6ea1955eedaff21b8df1178bc83c36
noemiepham <huynhtieuquy@gmail.com>
2022-11-22 15:15:08 +0100

    update city

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     name = Column(String(128), nullable = True)
     __tablename__ = 'cities'
