ee48615772f9e977a5c1f1e2c848b0ec4a246d14
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-22 12:10:54 +0100

    Delete object from __objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -54,3 +48,9 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """public instance method to delete obj from __objects"""
+        if obj:
+            del (FileStorage.__objects["{}.{}".format(
+                obj.__class__.__name__, obj.id)])
