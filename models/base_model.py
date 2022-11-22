0d84a3c72ec76e1dce790c3fad68994999cc63f6
noemiepham <huynhtieuquy@gmail.com>
2022-11-22 14:29:15 +0100

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import DateTime
 
+Base = declarative_base();
 
 class BaseModel:
+    """Defines the BaseModel class.
+            class attribute id
+            class attribute created_at
+            class attribute updated_at
+    """
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable = False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable = False, default=datetime.utcnow())
+
+
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if not kwargs or ("updated_at" not in kwargs and
+                          "created_at" not in kwargs):
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            pass
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +48,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +59,9 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         return dictionary
+    def delete(self):
+        """delete the current instance from the storage"""
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    name = Column(String(128), nullable = True)
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable = True)
+
+
