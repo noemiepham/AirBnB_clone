a85aa6c4d5e38c661ac7e1164ba657a46fee3933
noemiepham <huynhtieuquy@gmail.com>
2022-11-24 15:03:21 +0100

    update task 10

--- models/place.py
+++ models/place.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, Integer, Float
-from sqlalchemy import ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from os import environ
-from models import storage
-from models.city import City
+from os import environ, getenv
+from models import storage, Table
+from models.review import Review
+from models.amenity import Amenity
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             nullable=False, primary_key=True),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenity.id"),
+                             nullable=False, primary_key=True))
 
 
 class Place(BaseModel, Base):
@@ -25,28 +32,41 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=False)
     amenity_ids = []
 
-    #task 9: class attribute reviews must represent a relationship with the class Review
+    # task 9: class attribute reviews must represent a relationship with the class Review
 
-    if environ.get("HBNB_TYPE_STORAGE") == "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref="place",
                                cascade="all,delete")
-        
-        #tast 10: 
-        # Add an instance of SQLAlchemy Table called place_amenity for 
-        # creating the relationship Many-To-Many between Place and Amenity:
-        amenities = relationship("Amenity", backref="place_amenity",
-                                secondary="place_amenity", viewonly=False)
     else:
         @ property
-        def cities(self):
-            """getter method for cities"""
-            cities_dict = []
-            for key in storage.all(City):
-                if self.id in storage.all(City)[key].state_id:
-                    cities_dict.append(storage.all(City)[key])
-                return cities_dict
-    
-    #tast 10: 
-    # Add an instance of SQLAlchemy Table called place_amenity for 
-    # creating the relationship Many-To-Many between Place and Amenity:
+        def reviews(self):
+            """getter method for reviews"""
+            reviews_list = []
+            all_review = storage.all(Review)
+            for element in all_review.values():
+                if element.place_id == self.id:
+                    reviews_list.append(element)
+                return reviews_list
 
+    # tast 10:
+    # Add an instance of SQLAlchemy Table called place_amenity for
+    # creating the relationship Many-To-Many between Place and Amenity:
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        amenities = relationship("Amenity", backref="place_amenity",
+                                 secondary=place_amenity, viewonly=False)
+    else:
+        @property
+        def amenities(self):
+            """getter method for reviews"""
+            amenities_list = []
+            all_amenities = storage.all(Review)
+            for element in all_amenities.values():
+                if element.place_id == self.id:
+                    amenities_list.append(element)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, cls):
+            if not isinstance(cls, Amenity):
+                return
+            self.amenity_ids.append(cls.id)
