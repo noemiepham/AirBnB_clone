1d153c9e2a3dc270e14f1f26840139e57098f6f0
noemiepham <huynhtieuquy@gmail.com>
2022-11-24 13:20:35 +0100

    update task 8

--- models/place.py
+++ models/place.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(String(1024), nullable=False)
+    number_bathrooms = Column(int, nullable=False, defaut=0)
+    max_guest =Column(int, nullable=False, defaut=0)
+    price_by_night = Column(int, nullable=False, defaut=0)
+    latitude = Column(float, nullable=False)
+    longitude = Column(float, nullable=False)
     amenity_ids = []
+
+
+    # if environ.get("HBNB_TYPE_STORAGE") == "db":
+    #     reviews = relationship("Review", backref="place",
+    #                            cascade="all,delete")
