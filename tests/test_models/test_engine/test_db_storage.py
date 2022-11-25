9aab8fe8b0e088fb1c651b82f9637f95a0e83d2c
ChongLeangUENG <leangcnrp777@gmail.com>
2022-11-25 14:38:00 +0100

    Update all tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,86 +1,41 @@
 #!/usr/bin/python3
-"""test for file storage"""
+"""
+Unit tests for tha database storage.
+"""
 import unittest
-import pep8
-import json
-import os
-from os import getenv
-import MySQLdb
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+import pycodestyle
 from models.engine.db_storage import DBStorage
 
 
-@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-class TestDBStorage(unittest.TestCase):
-    '''this will test the DBStorage'''
+class test_db_storage(unittest.TestCase):
+    """class to test the db_storage method"""
 
-    @classmethod
-    def setUpClass(self):
-        """set up for test"""
-        self.User = getenv("HBNB_MYSQL_USER")
-        self.Passwd = getenv("HBNB_MYSQL_PWD")
-        self.Db = getenv("HBNB_MYSQL_DB")
-        self.Host = getenv("HBNB_MYSQL_HOST")
-        self.db = MySQLdb.connect(host=self.Host, user=self.User,
-                                  passwd=self.Passwd, db=self.Db,
-                                  charset="utf8")
-        self.query = self.db.cursor()
-        self.storage = DBStorage()
-        self.storage.reload()
+    def test_doc(self):
+        """
+        Check all the doc of the Amenity Class
+        """
+        # module documentation
+        module = len(DBStorage.__doc__)
+        self.assertGreater(module, 0)
 
-    @classmethod
-    def teardown(self):
-        """at the end of the test this will tear it down"""
-        self.query.close()
-        self.db.close()
+        # class documentation
+        module_class = len(DBStorage.__doc__)
+        self.assertGreater(module_class, 0)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_pep8_DBStorage(self):
-        """Test Pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        module_class = len(DBStorage.new.__doc__)
+        self.assertGreater(module_class, 0)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_read_tables(self):
-        """existing tables"""
-        self.query.execute("SHOW TABLES")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 7)
+        module_class = len(DBStorage.save.__doc__)
+        self.assertGreater(module_class, 0)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_no_element_user(self):
-        """no elem in users"""
-        self.query.execute("SELECT * FROM users")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 0)
+        module_class = len(DBStorage.delete.__doc__)
+        self.assertGreater(module_class, 0)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_no_element_cities(self):
-        """no elem in cities"""
-        self.query.execute("SELECT * FROM cities")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 0)
+        module_class = len(DBStorage.reload.__doc__)
+        self.assertGreater(module_class, 0)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_add(self):
-        """Test same size between storage() and existing db"""
-        self.query.execute("SELECT * FROM states")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 0)
-        state = State(name="LUISILLO")
-        state.save()
-        self.db.autocommit(True)
-        self.query.execute("SELECT * FROM states")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 1)
+        module_class = len(DBStorage.all.__doc__)
+        self.assertGreater(module_class, 0)
 
-
-if __name__ == "__main__":
-    unittest.main()
+        module_class = len(DBStorage.__init__.__doc__)
+        self.assertGreater(module_class, 0)
