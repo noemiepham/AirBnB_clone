6681967bd111b2c77e94b2193d2b7d7f818be001
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 06:31:14 +0100

    display “HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """web application must be listening on 0.0.0.0, port 5000"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print HBNB"""
+    return "HBNB"
+
+@app.route('/c/hbnb', strict_slashes=False)
+def hbnb1():
+    """print HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
