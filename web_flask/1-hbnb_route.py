fe381cd8cf7672e438f4bd8f8fa7284ddb2c023a
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 20:15:34 +0100

    pep 8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,16 +5,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hbnb():
     """web application must be listening on 0.0.0.0, port 5000"""
     return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb1():
     """print HBNB"""
     return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
+    app.url_map.strict_slashes = False
+    app.run('0.0.0.0', 5000, debug=True)
