af043dc6464a1f62e079d4a0141024c3205d1e9f
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 21:12:25 +0100

    module error

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application:"""
 from flask import Flask, render_template
-from waitress import serve
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hbnb():
     """web application must be listening on 0.0.0.0, port 5000"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb1():
     """print HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def hbnb2(text):
     """print c + text"""
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', defaults={"text": "is cool"}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', defaults={"text": "is cool"})
+@app.route('/python/<text>')
 def hbnb3(text):
     """print text is cool + text"""
     return "Python {:s}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>')
 def hbnb4(n):
     """print n is integer"""
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def hbnb5(n):
     """print n in h1"""
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
     """print n in h1"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
-    serve(app.run('0.0.0.0', 5000, debug=True))
+    app.url_map.strict_slashes = False
+    app.run('0.0.0.0', 5000, debug=True)
