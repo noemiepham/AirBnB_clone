6bc63751c515ca2f5490d5265b06395b59ad367f
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 05:11:00 +0100

    hello flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhthon3
+from flask import Flask
+from gevent.pywsgi import WSGIServer
+strict_slashes = False
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello HBNB"
+
+
+if __name__ == '__main__':
+    from waitress import serve
+
+    app.run(host="0.0.0.0", port=5000, debug = True) #host="0.0.0.0"
