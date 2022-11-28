96c5dd85cc49da51f02ab56b9b8e3de112844c3c
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 21:36:32 +0100

    list all serve

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+from flask import Flask, render_template
+from waitress import serve
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def handle_teardown():
+    """handle teardown"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def hbnb7():
+    """return all database in the db"""
+    state = storage.all(state)
+    return render_template('7-states_list.py', state=state)
+
+
+if __name__ == '__main__':
+    serve(app.run('0.0.0.0', 5000, debug=True))
