0fe7469891dc288db3dbcceabdeb546f1bb9c790
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 08:24:19 +0100

    update template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """web application must be listening on 0.0.0.0, port 5000"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb1():
+    """print HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb2(text):
+    """print c + text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hbnb3(text):
+    """print text is cool + text"""
+    return "Python {:s}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hbnb4(n):
+    """print n is integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def hbnb5(n=None):
+    """print n is integer"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
