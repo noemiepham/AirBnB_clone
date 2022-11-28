55ddfbb78c3ce1e7509e3d034d2efa129feecee3
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 06:44:20 +0100

    print c

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,7 +2,6 @@
 """Write a script that starts a Flask web application:"""
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
@@ -11,20 +10,18 @@ def hbnb():
     """web application must be listening on 0.0.0.0, port 5000"""
     return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """print HBNB"""
-    return "HBNB"
 
-@app.route('/c/hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb1():
     """print HBNB"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def hbnb2(text):
     """print c + text"""
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
