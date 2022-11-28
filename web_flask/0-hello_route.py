4364cad1b886f860d931ca01fafb9fc72ed0a6a5
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-28 05:41:44 +0100

    Fix erro WARNING: This is a development server.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
 from flask import Flask
-from gevent.pywsgi import WSGIServer
-strict_slashes = False
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """web application must be listening on 0.0.0.0, port 5000"""
     return "Hello HBNB"
 
 
 if __name__ == '__main__':
-    from waitress import serve
-
-    app.run(host="0.0.0.0", port=5000, debug=True)  # host="0.0.0.0"
+    app.run('0.0.0.0', 5000, debug=True)  # host="0.0.0.0"
