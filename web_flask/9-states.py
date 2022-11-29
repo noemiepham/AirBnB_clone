02ddcbb71443989a14ead31a756575cd4d894ee8
Noémie Huynh <huynhtieuquy@gmail.com>
2022-11-29 07:56:55 +0100

    check module

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application:"""
 from flask import Flask, render_template
-from models import storage
-from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
