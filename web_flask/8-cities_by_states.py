50e6812fc402773bdd90b791df0b4562c3aac160
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-01 12:41:09 +0100

    corriger code comme Moustapha

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application:"""
+"""Displays a list of states and cities using Flask"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from os import getenv
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def handle_teardown():
-    """handle teardown"""
-    storage.close()
-
-
-@app.route('/cities_by_states')
-def cities_by_states():
-    """return all database in the db"""
-    states = storage.all(State).values()
+@app.route("/cities_by_states")
+def list_states():
+    states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == '__main__':
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
     app.url_map.strict_slashes = False
-    app.run('0.0.0.0', 5000)
+    app.run(host="0.0.0.0", port=5000, use_reloader=True)
