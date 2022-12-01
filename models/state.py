a624b44621c0db9d3e4da6693e9944278a7a8f87
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-01 13:17:32 +0100

    update state

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for  HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
 from models import storage
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
