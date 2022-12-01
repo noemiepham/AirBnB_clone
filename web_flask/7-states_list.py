4bc445f2ebf7e5c571250906ea277f32886c5ee8
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-01 13:19:44 +0100

    state update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,8 +3,8 @@
 
 from flask import Flask
 from flask import render_template
-from models import storage
-#from models.state import State
+#from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
