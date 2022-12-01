f44f6940e17c46eed28423baff70bcdac603e112
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-01 12:24:17 +0100

    retire debug

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,8 +2,7 @@
 """Write a script that starts a Flask web application:"""
 
 from flask import Flask, render_template
-#from models import storage
-#from models.state import State
+import models
 
 app = Flask(__name__)
 
@@ -11,13 +10,13 @@ app = Flask(__name__)
 @app.teardown_appcontext
 def handle_teardown(exception):
     """handle teardown"""
-    storage.close()
+    models.storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """return all database in the db"""
-    state = storage.all(State).values()
+    state = models.storage.all(models.State).values()
     return render_template('7-states_list.html', state=state)
 
 
